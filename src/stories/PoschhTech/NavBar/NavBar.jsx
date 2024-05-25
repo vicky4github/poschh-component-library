81b3637341831ef765e8dcf3ce6b6efdedeee5f5
vicky-zluri <vy4693@gmail.com>
2024-05-25 21:47:26 +0530

    removing responsive modal

--- src/stories/PoschhTech/NavBar/NavBar.jsx
+++ src/stories/PoschhTech/NavBar/NavBar.jsx
@@ -2,7 +2,6 @@ import React,{useState} from 'react';
 import { Link, Button } from '@nextui-org/react';
 import './navbar.css';
 import { Dropdown } from '../Dropdown/Dropdown';
-import { Modal } from "react-responsive-modal";
 
 export const NavigationBar = () => {
   const [open,setOpen]=useState(false);
@@ -26,14 +25,7 @@ export const NavigationBar = () => {
         <Link>
          <Button id='salesButton' onClick={openForm}>Contact Sales</Button>
         </Link>
-        <Modal open={open} onClose={close}>
-          <h2>Simple centered modal</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-            pulvinar risus non risus hendrerit venenatis. Pellentesque sit amet
-            hendrerit risus, sed porttitor quam.
-          </p>
-        </Modal>
+       
        
       </div>
     </nav>
